72d3dcb22614c2ef886b951e80f94c6d06fff5e9
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-12-04 14:11:57 -0800

    Finish day 1 part 1

--- p1a_trebuchet/src/main.rs
+++ p1a_trebuchet/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../sample_input").split("\n");
+    let mut running_sum = 0;
+
+    for data in input {
+        for character in data.chars() {
+            if character.is_digit(10) {
+                running_sum = running_sum + character.to_digit(10).unwrap() * 10;
+                break;
+            }
+        }
+
+        for character in data.chars().rev() {
+            if character.is_digit(10) {
+                running_sum = running_sum + character.to_digit(10).unwrap();
+                break;
+            }
+        }
+    }
+
+    println!("{running_sum}")
 }

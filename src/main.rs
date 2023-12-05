a25390e2ca88f26cde16ae5333cae77ad4aa6238
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-12-05 13:40:14 -0800

    Add prompt for problem runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Advent of Code 2023");
+
+    println!("Input problem to run:");
+
+    let mut problem = String::new();
+    io::stdin()
+        .read_line(&mut problem)
+        .expect("Failed to read problem number");
+
+    println!("Running problem: {problem}...");
 }
